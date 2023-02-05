0a63de769c9b0a1bdc773fe6c310e768f37a298e
Zine-eddineBardoud <zineddinebardoud38@gmai.com>
2023-02-05 21:28:53 +0100

    ReactJS_Portfolio

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
+import Testimonial from './components/testimonials/Testimonial';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import ScrollUp from './components/scrollup/ScrollUp';
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main className='main'>
+        <Home />
+        <About />
+        <Skills />
+        <Services />
+        <Qualification />
+        <Testimonial />
+        <Contact />
+      </main>
+      
+      <Footer />
+      <ScrollUp />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './Header.css'
+
+const Header = () => {
+    /* -------------------- Change Active Header Color -------------------- */
+    window.addEventListener('scroll', function () {
+        const header = document.querySelector('.header');
+        if (this.scrollY >= 80 ) header.classList.add('scroll-header')
+        else header.classList.remove('scroll-header');
+    }) 
+    /* -------------------- Toggle Menu -------------------- */
+    const[Toggle, showMenu] = useState();
+    const [activeNav, setActiveNav] = useState('#about');
+
+    return (
+        <header className='header'>
+            <nav className='nav container'>
+                <a href='index.html' className='nav__logo'>Smith</a>
+
+                <div className={Toggle ? 'nav__menu show-menu' : 'nav__menu'}>
+                    <ul className='nav__list grid'>
+                        <li className='nav__item'>
+                            <a href='#home' onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'nav__link active-link' : 'nav__link'}>
+                                <i class="uil uil-estate nav__icon"></i> Home
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'nav__link active-link' : 'nav__link'}>
+                                <i class="uil uil-user nav__icon"></i> About
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#skills' onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'nav__link active-link' : 'nav__link'}>
+                                <i class="uil uil-file-alt nav__icon"></i> Skills
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'nav__link active-link' : 'nav__link'}>
+                                <i class="uil uil-briefcase-alt nav__icon"></i> Services
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#qualification' onClick={() => setActiveNav('#qualification')} className={activeNav === '#qualification' ? 'nav__link active-link' : 'nav__link'}>
+                                <i class="uil uil-scenery nav__icon"></i> Portfolio
+                            </a>
+                        </li>
+                        <li className='nav__item'>
+                            <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'nav__link active-link' : 'nav__link'}>
+                                <i class="uil uil-message nav__icon"></i> Contact
+                            </a>  
+                        </li>
+                    </ul>
+
+                    <i class='uil uil-times nav__close' onClick={() => showMenu(!Toggle)}></i>
+                </div>
+                <div className='nav__toggle' onClick={() => showMenu(!Toggle)}>
+                    <i class='uil uil-apps'></i>
+                </div> 
+                
+            </nav>
+        </header>
+    )
+};
+
+export default Header;
